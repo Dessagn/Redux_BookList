7b14423acd65346c41f75053ebf358042a12cb4f
Dessagn <temesgen.dessalegn@gmail.com>
2017-10-22 13:41:57 +0300

    Initial Commit

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascript: the Good Parts'},
+    {title: 'The dark Tower'},
+    {title: 'Merry Jane Watson'},
+    {title: 'Into the Blue'},
+    {title: 'The Five Brothers'}
+  ];
+}
