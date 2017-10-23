6df9a685468d4475d266bb534d8b0147ce236e10
Dessagn <temesgen.dessalegn@gmail.com>
2017-10-23 12:57:17 +0300

    Add book details container and reducer

---
+++ src/containers/BookDetail.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import reducer-bookDetail from '../reducers/reducer-bookDetail';
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default BookDetail;
